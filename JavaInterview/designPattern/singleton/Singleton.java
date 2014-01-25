8a7ecbe323de322299f93a347c5e015a211b6838
yshynwq <yshynwq@gmail.com>
2014-01-24 23:36:30 -0500

    singleton

---
+++ JavaInterview/designPattern/singleton/Singleton.java
@@ -0,0 +1,13 @@
+package singleton;
+
+public class Singleton {
+	private static Singleton instance;
+	private Singleton() {
+	}
+	public synchronized static Singleton getInstance(){
+		if(instance==null){
+			instance = new Singleton();
+		}
+		return instance;
+	}
+}
